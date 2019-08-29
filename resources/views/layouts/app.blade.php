7ed01205c594791e90083d071f0d49d20ef407ae
pc <mohamedkhalil1997@hotmail.fr>
2019-08-29 18:13:58 +0100

    background img

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,12 +6,11 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
     <title>Document</title>
-
     <link rel="stylesheet" href="{{ asset('css/cards.css') }}">
     <link rel="stylesheet" href="{{ asset('css/userprofile.css')}}">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
-    <link rel="stylesheet" href="{{ asset('css/layout.css')}}">
+    <link rel="stylesheet" href="{{asset('css/layout.css')}}">
 </head>
 <body>
 
@@ -42,10 +41,4 @@ $('.carousel').carousel({
 document.getElementById("nom").removeAttribute('disabled');
 </script>
 </body>
-<style>
-    #intro{
-        background: url("/storage/pictures/main.jpg");
-        background-size: cover;
-    }
-</style>
 </html>
